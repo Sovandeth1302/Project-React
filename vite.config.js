70fbd08061ff04068122e8d9ef58f508b9f89c2e
Sovandeth1302 <cheom.sovandeth13@gmail.com>
2025-01-04 23:48:28 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 80,
+    host: '0.0.0.0' // Allows the server to be accessed from any network interface
+  },
   plugins: [react()],
   css: {
     devSourcemap: false
   },
-  server: {
-     host: '0.0.0.0,
-     port: 80,
-  }
 })
 
   
